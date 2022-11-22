171169270438c7d7b8e0217b2fb1e48f6be07c4a
BoucettaKhadidja <meriamem2@gmail.com>
2022-11-22 18:28:48 +0100

    DBConnection_Singleton

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,23 +1,35 @@
 import java.sql.Connection;
+
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DBConnection {
 	   
-		String BDD = "nomBD";
-		String url = "jdbc:mysql://localhost:3306/" + BDD;
-		String user = "root";
-		String passwd = "";
+		private String BDD = "nomBD";
+		private String url = "jdbc:mysql://localhost:3306/" + BDD;
+		private String user = "root";
+		private String passwd = "";
 	    private Connection conn;
+	  
 
 	   
 	    public DBConnection() throws SQLException {
-			conn=DriverManager.getConnection(url, user,passwd);
+	    	try {
+	    		conn=DriverManager.getConnection(url, user,passwd);
+	    	}catch (SQLException ex) {
+	    		Logger.getLogger(BDD).log(Level.SEVERE, null, ex);
+	    	}
+	    
 		}
 
 	    
-	    public Connection getConn() {
-			return conn;
+    public  synchronized Connection getConn() throws SQLException {
+		if (conn == null) {
+		     new DBConnection();
+		}
+    	return conn;
 		}
 
 
